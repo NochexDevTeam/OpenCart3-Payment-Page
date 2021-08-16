a0059c75f5626a9e6f984af8bd0ddeda29cdebe9
NochexDevTeam <36759878+NochexDevTeam@users.noreply.github.com>
2021-08-16 15:32:52 +0100

    Update nochex.php

--- catalog/language/en-gb/extension/payment/nochex.php
+++ catalog/language/en-gb/extension/payment/nochex.php
@@ -1,3 +1,3 @@
 <?php
 // Text
-$_['text_title'] = 'Credit Card / Debit Card (NOCHEX) <br/> <img src="image/nochex-cards.png" width="250px" />';
+$_['text_title'] = 'Credit Card / Debit Card (NOCHEX) <br/> <img src="image/nochexcards.png" width="250px" />';
